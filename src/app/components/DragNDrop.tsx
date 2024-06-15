d082aa5b8eb9d66b1e5a460f6063d9e3f876b353
Neripheral <neripheral@gmail.com>
2024-06-15 22:15:08 +0200

    feat: flesh out the drop area's style

--- src/app/components/DragNDrop.tsx
+++ src/app/components/DragNDrop.tsx
@@ -1,8 +1,17 @@
+import Image from 'next/image';
+import dropIcon from '/public/drop-icon.png'
+
 export default function DragNDrop(){
     return (
-        <div className="bg-gradient-to-r from-white/20 via-gray-700/20 to-white/20 m-5 p-5 rounded-lg">
-            <form>
-                <p>Select File:</p>
+        <div className="transition border-4 border-gray-400/40 border-dashed bg-black/30 hover:bg-black/20 hover:scale-110 aspect-square m-5 p-5 rounded-3xl">
+            <form className="min-h-full flex">
+                <Image
+                    src={dropIcon}
+                    alt="Drop your file here."
+                    width={200}
+                    height={200}
+                    className="opacity-40 m-auto"
+                    />
                 <input type="file" className="hidden"/>
                 <button type={"submit"} className="hidden">Upload</button>
             </form>
