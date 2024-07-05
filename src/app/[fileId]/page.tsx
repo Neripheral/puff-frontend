6166e7d6f07cc417e3c19b7adc26e487d4ac0ddd
Neripheral <neripheral@gmail.com>
2024-07-05 22:26:37 +0200

    feat: add simple File page

---
+++ src/app/[fileId]/page.tsx
@@ -0,0 +1,24 @@
+import Database from "@/lib/Database";
+
+let fileId = 0;
+
+async function getFilename(){
+    return Database.getFileData(fileId);
+}
+
+export default async function Page({params}: {params: {fileId: number}}){
+    fileId = params.fileId;
+    const filename = await getFilename();
+
+    return (
+        <main className="px-8">
+            <section className="mx-auto p-5 text-white text-center ">
+                <h1 className="font-sans text-5xl font-extrabold py-4">Get droplet:</h1>
+                <h2 className="text-2xl">{filename}</h2>
+            </section>
+            <section className="w-1/3 mx-auto">
+
+            </section>
+        </main>
+    );
+}
