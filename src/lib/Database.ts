5536e5f3f26595b16ed5b5c16db9725ef5b07d04
Neripheral <neripheral@gmail.com>
2024-07-05 22:26:14 +0200

    feat: add filename fetching to Database

--- src/lib/Database.ts
+++ src/lib/Database.ts
@@ -16,6 +16,31 @@ class Database {
             console.log(JSON.stringify(response));
         });
     }
+
+    async getFileData(id: number){
+        const res = await fetch(this.rootUrl + id, {
+            method: "HEAD",
+            cache: "force-cache"
+        });
+
+        const filename = this.getFilenameFromHeader(res.headers.get("Content-Disposition"));
+        if(!filename) {
+            return null;
+        }
+        return filename;
+    }
+
+    private getFilenameFromHeader(header: string|null){
+        if(!header)
+            return null;
+
+        const regex = new RegExp(`^attachment; filename="([^"]*)"$`);
+        const match = header.match(regex);
+
+        if(match && match[1])
+            return match[1];
+        return null;
+    }
 }
 
 export default new Database(`http://localhost:8080/puff/api/`);
