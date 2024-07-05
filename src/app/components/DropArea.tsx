f2ec88ca6dd5c3569ba970697455e9260301c688
Neripheral <neripheral@gmail.com>
2024-07-05 18:12:44 +0200

    feat: add drag'n'drop functionality to DropArea

--- src/app/components/DropArea.tsx
+++ src/app/components/DropArea.tsx
@@ -1,9 +1,41 @@
+'use client'
+
 import Image from 'next/image';
 import dropIcon from '/public/drop-icon.png'
+import {useState} from "react";
 
 export default function DropArea(){
+    const [file, setFile] = useState<File>();
+    const [fileEnter, setFileEnter] = useState(false);
+
     return (
-        <div className="transition border-4 border-gray-400/40 border-dashed bg-black/30 hover:bg-black/20 hover:scale-110 aspect-square m-5 p-5 rounded-3xl">
+        <div className={(fileEnter ? "bg-black/20 scale-110" : "bg-black/30 scale-100") + " transition border-4 border-gray-400/40 border-dashed aspect-square m-5 p-5 rounded-3xl"}
+            onDragOver={(e) => {
+                e.preventDefault();
+                setFileEnter(true);
+            }}
+            onDragLeave={(e)=>{
+                setFileEnter(false);
+            }}
+            onDragEnd={(e) => {
+                e.preventDefault();
+                setFileEnter(false);
+            }}
+            onDrop={(e)=>{
+                e.preventDefault();
+                setFileEnter(false);
+                if(e.dataTransfer.items) {
+                    [...e.dataTransfer.items].forEach((item, i) =>{
+                       if(item.kind === "file"){
+                           const file: File | null = item.getAsFile();
+                           if(file){
+                               setFile(file);
+                               uploadFile(file);
+                           }
+                       }
+                    });
+                }
+            }}>
             <form className="min-h-full flex">
                 <Image
                     src={dropIcon}
@@ -17,4 +49,16 @@ export default function DropArea(){
             </form>
         </div>
     );
+}
+
+function uploadFile(file: File){
+    console.log("Sending file");
+    const formData = new FormData();
+    formData.set("file", file);
+    fetch(`http://localhost:8080/puff/api/`, {
+        method: "POST",
+        body: formData
+    }).then(response=>{
+        console.log(JSON.stringify(response));
+    });
 }
